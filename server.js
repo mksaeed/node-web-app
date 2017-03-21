b96da30c146cd3d4b86dca904d93d58afefe851b
Muhammad Kamran Saeed <kamran.muhammad@outlook.com>
2017-03-22 01:42:56 +0500

    Setup start script and make heruko port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -59,8 +60,8 @@ app.get('/about', (req, res)=>{
 
 
 
-app.listen(3000, ()=> {
-     console.log('Server Start on port 3000!')
+app.listen(port, ()=> {
+     console.log(`Server Start on port ${port}!`)
 });
 
 
