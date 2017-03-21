d3da11b906715464c77958e9baa7092836edabbf
Muhammad Kamran Saeed <kamran.muhammad@outlook.com>
2017-03-22 01:29:42 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+
+//Setting up bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
+
+//Middleware saving server.log
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+
+//Middleware to use if site is under maintance 
+app.use((req, res, next)=>{
+    res.render('maintance.hbs',{
+        title: 'Under Maintance'
+    });
+});
+
+
+hbs.registerPartials(__dirname + '/views/layouts');
+app.set('view_engine', 'hbs');
+
+
+//Register Partial helpers
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+});
+
+
+//Register Home Page Route
+app.get('/', (req, res)=> {
+    res.render('home.hbs', {
+        title: 'Home Page',
+    });
+});
+
+
+//Register About Page Route
+app.get('/about', (req, res)=>{
+    res.render('about.hbs', {
+        title: 'About Us',
+    });
+});
+
+
+
+
+
+app.listen(3000, ()=> {
+     console.log('Server Start on port 3000!')
+});
+
+
+
+// app.get('/', function (req, res) {
+//   res.send('hello world')
+// })
+
+// app.get('/', (req, res) => {
+//     // res.send('<h1>Home Page</h1>');
+
+//     res.send({
+//         name : 'Andrew',
+//         likes : [
+
+//             'bikes',
+//             'Moorcars'
+
+//         ]
+
+
+//     });
+
+// });
+
+// app.get('/', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
+
+// POST method route
+// app.get('/about', function (req, res) {
+//   res.send('POST request to the About Page')
+// })
